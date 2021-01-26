6fd6c613bf17020b74a5d5cb9f906655b5daf35a
VRT36 <45532566+VRT36@users.noreply.github.com>
2021-01-26 16:35:03 +0530

    Update ss.js

--- js/ss.js
+++ js/ss.js
@@ -5,29 +5,29 @@ Time        : 11:09
 */
 function set(){
   sessionStorage.setItem("preset", "1");
-  document.getElementById("root").classList.add("preset-1");
+  document.getElementById("body").classList.add("preset-1");
 }
 // TS#1554010709
 function get(){
   var vrt = sessionStorage.getItem("preset");
   if (vrt == "1"){
     sessionStorage.setItem("preset", "2");
-    document.getElementById("root").classList.add("preset-2");
+    document.getElementById("body").classList.add("preset-2");
   }else if(vrt == "2"){
     sessionStorage.setItem("preset", "3");
-    document.getElementById("root").classList.add("preset-3");
+    document.getElementById("body").classList.add("preset-3");
   }else if(vrt == "3"){
     sessionStorage.setItem("preset", "4");
-    document.getElementById("root").classList.add("preset-4");
+    document.getElementById("body").classList.add("preset-4");
   }else if(vrt == "4"){
     sessionStorage.setItem("preset", "5");
-    document.getElementById("root").classList.add("preset-5");
+    document.getElementById("body").classList.add("preset-5");
   }else if(vrt == "5"){
     sessionStorage.setItem("preset", "6");
-    document.getElementById("root").classList.add("preset-6"); 
+    document.getElementById("body").classList.add("preset-6"); 
   }else if(vrt == "6"){
     sessionStorage.setItem("preset", "7");
-    document.getElementById("root").classList.add("preset-7");
+    document.getElementById("body").classList.add("preset-7");
   }else {
       set();
   }
